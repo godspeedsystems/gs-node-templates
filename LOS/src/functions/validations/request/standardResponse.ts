eca49e1b7ee656be70cf8584adec036ceede2430
mastersilv3r <ayush@godspeed.systems>
2024-02-29 03:37:51 +0530

    added response error handling functions

---
+++ LOS/src/functions/validations/request/standardResponse.ts
@@ -0,0 +1,9 @@
+import { GSContext } from "@godspeedsystems/core";
+
+export default function (ctx: GSContext) {
+    return {
+        success: false,
+        data: {"name": "ayush"},
+        code: 400
+    }
+}
