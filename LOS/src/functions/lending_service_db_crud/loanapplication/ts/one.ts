3f69f71b01ac59b97e5c836823d8cec80b0c5770
mastersilv3r <ayush@godspeed.systems>
2024-03-01 10:55:39 +0530

    schema updates

--- LOS/src/functions/lending_service_db_crud/loanapplication/ts/one.ts
+++ LOS/src/functions/lending_service_db_crud/loanapplication/ts/one.ts
@@ -5,5 +5,5 @@ module.exports = async(ctx: GSContext) => {
     const {params} = ctx.inputs.data;
     const {datasources}  = ctx;
     const res = await datasources.lending_service_db.client.LoanApplication.findUnique({where:{id:parseInt(params.id)}})
-    return new GSStatus(true, 200, 'Successfully fetched Loan Application by id.', res, undefined);
+    return new GSStatus(true, 200, 'Successfully fetched Loan Application by id.', res || {}, undefined);
 };
