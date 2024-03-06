35b3f1798499acb52ae7a599de49aa1557de1aa1
mastersilv3r <ayush@godspeed.systems>
2024-03-06 23:13:08 +0530

    removed salesforce

--- LOS/src/functions/lending_service_db_crud/loanapplication/ts/search.ts
+++ LOS/src/functions/lending_service_db_crud/loanapplication/ts/search.ts
@@ -1,8 +1,10 @@
 import { GSContext, GSStatus } from "@godspeedsystems/core";
+import { PrismaClient } from "@prisma/client";
 
 
 module.exports = async(ctx: GSContext) => {
     const {datasources}  = ctx;
-    const res = await datasources.lending_service_db.client.LoanApplication.findMany(ctx.inputs.data.body);
+    const client: PrismaClient = datasources.lending_service_db.client;
+    const res = await client.LoanApplication.findMany(ctx.inputs.data.body);
     return new GSStatus(true, 200, 'successfully fetched Loan Applications list', res, undefined);
 };
--- LOS/src/functions/validations/request/standardResponse.ts
+++ LOS/src/functions/validations/request/standardResponse.ts
@@ -1,9 +1,10 @@
-import { GSContext } from "@godspeedsystems/core";
+import { GSContext, logger } from "@godspeedsystems/core";
 
 export default function (ctx: GSContext) {
+    // logger.error(ctx.inputs.data.validation_error)
     return {
         success: false,
-        data: {"name": "ayush"},
+        data: ctx.inputs.data,
         code: 400
     }
 }
